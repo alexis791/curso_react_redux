a5a115b4a72de57b021ec55175048a6db39f35b8
Alexis <dalexisbr@gmail.com>
2019-11-15 15:39:09 -0700

    Redux - maStateToProps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**Components */
+import Details from './components/Details'
+import Results from './components/Results'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/** Store de redux */
+import store from './redux/store'
+
+const Root = () => (
+    
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/results" component={Results}/>
+                <Route path="/details/:idItem" component={Details}/>
+                <Redirect from="/" to="/results" />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+)
+
+ReactDOM.render(<Root/>, document.getElementById('root'))
